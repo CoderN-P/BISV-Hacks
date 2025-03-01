be4e479d8b42b701176cce1d6302faf5be786243
CoderN-P <neel.parpia@gmail.com>
2025-02-28 23:01:37 -0800

    commit build

--- frontend/build/server/index.js
+++ frontend/build/server/index.js
@@ -533,7 +533,7 @@ const options = {
   root,
   service_worker: false,
   templates: {
-    app: ({ head, body, assets: assets2, nonce, env }) => '<!doctype html>\n<html lang="en">\n	<head>\n		<meta charset="utf-8" />\n		<link rel="icon" href="' + assets2 + '/favicon.png" />\n		<link rel="stylesheet" href="' + assets2 + '/css/globals.css" />\n		<meta name="viewport" content="width=device-width, initial-scale=1" />\n		<title>Foodlyzer</title>\n		<meta name="description" content="Foodlyze is an AI-powered app that scans barcodes to analyze food ingredients, providing a healthiness score and an easy-to-understand explanation. By making food transparency accessible to all, Foodlyze empowers individuals, especially those in low-income communities, to make healthier, more informed choices about their diet."/>\n		<meta name="og:title" content="Foodlyzer" />\n		<meta name="og:description" content="Foodlyze is an AI-powered app that scans barcodes to analyze food ingredients, providing a healthiness score and an easy-to-understand explanation. By making food transparency accessible to all, Foodlyzer empowers individuals, especially those in low-income communities, to make healthier, more informed choices about their diet." />\n		\n		' + head + '\n	</head>\n	<body data-sveltekit-preload-data="hover">\n		<div style="display: contents">' + body + "</div>\n	</body>\n</html>\n",
+    app: ({ head, body, assets: assets2, nonce, env }) => '<!doctype html>\n<html lang="en">\n	<head>\n		<meta charset="utf-8" />\n		<link rel="icon" href="' + assets2 + '/favicon.png" />\n		<link rel="stylesheet" href="' + assets2 + '/css/globals.css" />\n		<meta name="viewport" content="width=device-width, initial-scale=1" />\n		<title>Foodlyze</title>\n		<meta name="description" content="Foodlyze is an AI-powered app that scans barcodes to analyze food ingredients, providing a healthiness score and an easy-to-understand explanation. By making food transparency accessible to all, Foodlyze empowers individuals, especially those in low-income communities, to make healthier, more informed choices about their diet."/>\n		<meta name="og:title" content="Foodlyze" />\n		<meta name="og:description" content="Foodlyze is an AI-powered app that scans barcodes to analyze food ingredients, providing a healthiness score and an easy-to-understand explanation. By making food transparency accessible to all, Foodlyze empowers individuals, especially those in low-income communities, to make healthier, more informed choices about their diet." />\n		\n		' + head + '\n	</head>\n	<body data-sveltekit-preload-data="hover">\n		<div style="display: contents">' + body + "</div>\n	</body>\n</html>\n",
     error: ({ status, message }) => '<!doctype html>\n<html lang="en">\n	<head>\n		<meta charset="utf-8" />\n		<title>' + message + `</title>
 
 		<style>
@@ -605,7 +605,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "17cv2ba"
+  version_hash: "1le4gri"
 };
 async function get_hooks() {
   let handle;
--- frontend/build/server/manifest.js
+++ frontend/build/server/manifest.js
@@ -10,11 +10,11 @@ return {
 	assets: new Set(["css/globals.css","favicon.png"]),
 	mimeTypes: {".css":"text/css",".png":"image/png"},
 	_: {
-		client: {start:"_app/immutable/entry/start.BmFMsnin.js",app:"_app/immutable/entry/app.Dp1vcXiw.js",imports:["_app/immutable/entry/start.BmFMsnin.js","_app/immutable/chunks/6d_m6pp3.js","_app/immutable/chunks/C_2bOysV.js","_app/immutable/chunks/BDLwdBMC.js","_app/immutable/entry/app.Dp1vcXiw.js","_app/immutable/chunks/C_2bOysV.js","_app/immutable/chunks/DasDVUsj.js","_app/immutable/chunks/Dtw3Ef1T.js","_app/immutable/chunks/CAApebIF.js","_app/immutable/chunks/BDLwdBMC.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.D2MuPijb.js",app:"_app/immutable/entry/app.DB_Ecm1c.js",imports:["_app/immutable/entry/start.D2MuPijb.js","_app/immutable/chunks/Cl5VadBT.js","_app/immutable/chunks/C_2bOysV.js","_app/immutable/chunks/BDLwdBMC.js","_app/immutable/entry/app.DB_Ecm1c.js","_app/immutable/chunks/C_2bOysV.js","_app/immutable/chunks/DasDVUsj.js","_app/immutable/chunks/Dtw3Ef1T.js","_app/immutable/chunks/CAApebIF.js","_app/immutable/chunks/BDLwdBMC.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./chunks/0-Bw7Go1yu.js')),
-			__memo(() => import('./chunks/1-B1BMFzfr.js')),
-			__memo(() => import('./chunks/2-BfZJQrgp.js'))
+			__memo(() => import('./chunks/1-CGlP_sqn.js')),
+			__memo(() => import('./chunks/2-CP1nj_U0.js'))
 		],
 		routes: [
 			{
